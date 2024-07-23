255c09b6efd0ed5601238fd8aaaa16f20ac7b9ab
jaomuro <jhonskaz@gmail.com>
2024-07-23 11:56:08 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export function App() {
+  return (
+    <>
+      <header></header>
+      <main>
+        <div>Primeiro</div>
+        <div>Segundo</div>
+        <div>Terceiro</div>
+      </main>
+    </>
+  )
+}
